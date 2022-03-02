fc78b276d9e808b21f845e1c0dc46194c6c8c4ea
RS <richard.alexander.schulz@gmail.com>
2022-03-02 18:24:01 +0100

    dont post news twice (if they appear in two different searches)

--- src/main/java/de/bambussoft/immopush/fetch/NewsFilter.java
+++ src/main/java/de/bambussoft/immopush/fetch/NewsFilter.java
@@ -5,6 +5,7 @@ import de.bambussoft.immopush.repo.UrlRepository;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
@@ -19,6 +20,32 @@ public class NewsFilter {
 
     public List<URL> filter(List<URL> strip, String chatId) {
         Map<String, List<URL>> urlsPerHost = strip.stream().map(URL::getHost).distinct().collect(toMap(host -> host, host -> strip.stream().filter(url -> url.getHost().equals(host)).collect(Collectors.toList())));
-        return urlsPerHost.keySet().stream().flatMap(h -> urlsPerHost.get(h).stream().filter(url -> !urlRepository.existsByUrlAndChatId(url.toString(), chatId))).collect(Collectors.toList());
+        List<URL> news = urlsPerHost.keySet().stream().flatMap(h -> urlsPerHost.get(h).stream().filter(url -> !urlRepository.existsByUrlAndChatId(url.toString(), chatId))).collect(Collectors.toList());
+        return news.stream().map(StingEqualsUrl::new).distinct().map(StingEqualsUrl::getUrl).collect(Collectors.toList());
+    }
+
+    static class StingEqualsUrl {
+        private final URL url;
+
+        public StingEqualsUrl(URL url) {
+            this.url = url;
+        }
+
+        public URL getUrl() {
+            return url;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            StingEqualsUrl that = (StingEqualsUrl) o;
+            return Objects.equals(url.toString(), that.url.toString());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(url.toString());
+        }
     }
 }
